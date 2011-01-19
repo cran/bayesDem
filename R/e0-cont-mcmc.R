27432af449ab64d66036291be35e6053435539c2
Hana Sevcikova <hanas@uw.edu>
2011-01-19 00:00:00 +0000

    version 1.2-0

--- R/e0-cont-mcmc.R
+++ R/e0-cont-mcmc.R
@@ -1,6 +1,6 @@
-TFRcontinueMCMCgroup <- function(g, main.win, parent=NULL) {
+e0ContinueMCMCgroup <- function(g, main.win, parent=NULL) {
 	e <- new.env()
-	defaults <- formals(continue.tfr.mcmc) # default argument values
+	defaults <- formals(continue.e0.mcmc) # default argument values
 
 	e$output.dir <- parent$sim.dir
 	mcmc.g <- gframe("<span color='blue'>MCMC</span>", markup=TRUE, horizontal=FALSE, cont=g)
@@ -10,13 +10,15 @@ TFRcontinueMCMCgroup <- function(g, main.win, parent=NULL) {
 							cont=auto.g, handler=function(h,...){.enable.auto.cont(svalue(h$obj), e)})
 	e$auto.conf.b <- gbutton(' Configure auto run ', cont=auto.g, handler=configure.auto.run, 
 				action=list(mw=main.win, env=e, cont.run=TRUE))
+	enabled(e$run.auto) <- FALSE
+	enabled(e$auto.conf.b) <- FALSE
 	iter.g <- ggroup(horizontal=TRUE, cont=mcmc.g)
 	glabel("Number of iterations:", cont=iter.g)
 	glabel("<span color='red'>*</span>", markup=TRUE, cont=iter.g)
 	e$iter <- gedit(defaults$iter, width=7, cont=iter.g)
 	glabel("Chain ids:", cont=iter.g)
 	e$chain.ids <- gedit(defaults$chain.ids, width=10, cont=iter.g)
-	.enable.auto.cont(FALSE, e)
+	#.enable.auto.cont(FALSE, e)
 	
 	paral.g <- gframe("<span color='blue'>Process control</span>", markup=TRUE,
 					 horizontal=TRUE, cont=g)
@@ -28,33 +30,18 @@ TFRcontinueMCMCgroup <- function(g, main.win, parent=NULL) {
 	
 	addSpring(g)
 	cont.g <- ggroup(horizontal=TRUE, cont=g)
-	create.help.button(topic='continue.tfr.mcmc', package='bayesTFR', parent.group=cont.g,
+	create.help.button(topic='continue.e0.mcmc', package='bayesLife', parent.group=cont.g,
 						parent.window=main.win)
 	addSpring(cont.g)
-	gbutton(' Generate Script ', cont=cont.g, handler=mcmc.continue,
+	gbutton(' Generate Script ', cont=cont.g, handler=e0mcmc.continue,
 				action=list(mw=main.win, env=e, script=TRUE))
-	gbutton(action=gaction(label=' Continue MCMC ', icon='execute', handler=mcmc.continue, 
+	gbutton(action=gaction(label=' Continue MCMC ', icon='execute', handler=e0mcmc.continue, 
 				action=list(mw=main.win, env=e, script=FALSE)), cont=cont.g)
 	return(e)
+
 }
 
-.enable.auto.cont <- function(enable, e) {
-	enabled(e$auto.conf.b) <- enable
-	enabled(e$chain.ids) <- !enable
-	enabled(e$iter) <- !enable 
-}
-
-.get.defaults.for.auto.cont <- function(e) {
-	mcmc.set <- get.tfr.mcmc(sim.dir=svalue(e$output.dir))
-	if(is.null(mcmc.set)) {
-		gmessage('Simulation directory contains no valid TFR MCMCs.', title='Input Error',
-					icon='error')
-		return(NULL)
-	}
-	return(mcmc.set$meta$auto.conf)
-}
-
-mcmc.continue <- function(h, ...) {
+e0mcmc.continue <- function(h, ...) {
 	e <- h$action$env
 	if(!has.required.arguments(list(output.dir='Simulation directory'), env=e)) return()
 	run.auto <- svalue(e$run.auto)
@@ -70,15 +57,15 @@ mcmc.continue <- function(h, ...) {
 		params[['iter']] <- if (h$action$script) sQuote('auto') else 'auto'
 	}
 	if (h$action$script) {
-		script.text <- gwindow('bayesTFR commands', parent=h$action$mw)
-		commands <- paste('m <- continue.tfr.mcmc(', paste(paste(names(params), params, sep='='), collapse=', '),
+		script.text <- gwindow('bayesLife commands', parent=h$action$mw)
+		commands <- paste('m <- continue.e0.mcmc(', paste(paste(names(params), params, sep='='), collapse=', '),
 											 ')',sep=' ')
-		if(run.auto && e$run.prediction) 
-			commands <- paste(commands, '\n\ntfr.predict(m, use.diagnostics=TRUE, replace.output=TRUE)', sep='')
+		#if(run.auto && e$run.prediction) 
+		#	commands <- paste(commands, '\n\ne0.predict(m, use.diagnostics=TRUE, replace.output=TRUE)', sep='')
 		gtext(commands, cont=script.text)
 	} else {
-		m <- do.call('continue.tfr.mcmc', params)
-		if(run.auto && e$run.prediction)
-			tfr.predict(m, use.diagnostics=TRUE, replace.output=TRUE)
+		m <- do.call('continue.e0.mcmc', params)
+		#if(run.auto && e$run.prediction)
+		#	e0.predict(m, use.diagnostics=TRUE, replace.output=TRUE)
 	}
 }
